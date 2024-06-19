a9e3e16d65073d47888f6cfc35a1317d78f40de1
TheCoinMaster <88158301+TheCoinMaster@users.noreply.github.com>
2024-06-19 08:13:19 -0400

    Update index.js

--- src/pages/dashboard/components/description/index.js
+++ src/pages/dashboard/components/description/index.js
@@ -8,7 +8,7 @@ const Description = () => {
         <p style={styles.descriptionTxt}>{`Qartel is the first casino and NFT collection on Qom. Our fast, secure, marketplace houses the syndicate. Collect and trade—Intimidate rivals and inspire loyalty, solidifying your place in the hierarchy of power.`}</p>
       </div>
       <div style={styles.description2}>
-        <p style={styles.descriptionTxt}>{`Qartel has an exclusive collection of 2222 NFTs, each representing a unique character. Among these digital assets, hawks symbolize keen strategists, always on the lookout for opportunities. Mules, dependable and sturdy, carry the weight of crucial missions. Bosses, a lot of power, the directors of operations. Sicarios, fierce enforcers, ensuring tasks are executed with precision.`}</p>
+        <p style={styles.descriptionTxt}>{`Qartel has an exclusive collection of 2222 NFTs, each representing a unique character. Among these digital assets, hawks symbolize keen strategists, always on the lookout for opportunities.`}</p>
       </div>
     </>
   );
