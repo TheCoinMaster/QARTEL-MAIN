c1ba80f8e019847a26522be81988e01c50a51b79
TheCoinMaster <88158301+TheCoinMaster@users.noreply.github.com>
2024-07-09 23:04:41 -0400

    Update index.js

--- src/pages/dashboard/components/services/index.js
+++ src/pages/dashboard/components/services/index.js
@@ -61,12 +61,12 @@ const Services = () => {
       <div style={styles.brandWrap}>
         <span id="brand" style={styles.brand}>
         <ul>
-  <h1>Phase One:</h1>
-  <li>2222 NFT Mint</li>
-  <li>Token Launch</li>
-  <li>QARTEL Slots</li>
-  <li>NFT Staking. Boosted QOM Pool/Casino Revenue</li>
-  <li>Token Staking For QOM Pool</li>
+  <h3>Phase One:</h3>
+  <li>- 2222 NFT Mint</li>
+  <li>- Token Launch</li>
+  <li>- QARTEL Slots</li>
+  <li>- NFT Staking. Boosted QOM Pool/Casino Revenue</li>
+  <li>- Token Staking For QOM Pool</li>
 </ul>
         </span>
       </div>
